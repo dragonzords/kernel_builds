0f79b11e7fd5522d6698fa9af7ae4884659d0ed6
Hnkksk <xyzuan1608@gmail.com>
2025-06-23 01:13:44 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 export ARCH="arm64"
 export DEVICE
-export KBUILD_BUILD_USER="wulan17"
+export KBUILD_BUILD_USER="Lynx"
 export KBUILD_BUILD_HOST="Github"
 export CLANG_URL="https://github.com/Mayuri-Chan/clang/releases/download/21.0.0git-e64f8e043/Mayuri-clang_21.0.0git-bookworm-adfea33f0.tar.xz"
 export CLANG_NAME="$(dirname "$(realpath "$0")")"
@@ -15,7 +15,7 @@ export PATH="$BASE_DIR/clang/bin:/usr/lib/ccache:$PATH"
 export IS_SUSFS
 
 if [ $IS_SUSFS -eq 1 ]; then
-    export ZIP_NAME="Mayuri-kernel-$DEVICE-rksu-susfs-"$(env TZ='Asia/Jakarta' date +%Y%m%d)
+    export ZIP_NAME="Thunderstorm-kernel-$DEVICE-rksu-susfs-"$(env TZ='Asia/Jakarta' date +%Y%m%d)
 else
-    export ZIP_NAME="Mayuri-kernel-$DEVICE-rksu-"$(env TZ='Asia/Jakarta' date +%Y%m%d)
+    export ZIP_NAME="Thunderstorm-kernel-$DEVICE-rksu-"$(env TZ='Asia/Jakarta' date +%Y%m%d)
 fi
